3f8caa4187ab79a3e7b2c94f7ece30cec413664e
MohammedAlfateh2 <114475891+MohammedAlfateh2@users.noreply.github.com>
2022-09-27 12:40:05 +0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7fad9ae9ae4f30b2eb2936b61b884a49",
-"/": "7fad9ae9ae4f30b2eb2936b61b884a49",
+"index.html": "a21d122dc5222409f26b83726ac73216",
+"/": "a21d122dc5222409f26b83726ac73216",
 "main.dart.js": "0028dbfb21a104452ce8933415ae1e33",
 "manifest.json": "9a0de1ff26661cd281cb9deb3db33b02",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
